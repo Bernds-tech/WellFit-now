23571a402ae84e900613fdc1e83c140bda102716
Bernds-tech <b.guggennberger@gmail.com>
2026-04-28 17:22:47 +0200

    Add missions type barrel exports

---
+++ lib/missions/index.ts
@@ -0,0 +1,17 @@
+export type {
+  MissionCategoryDefinition,
+  WellFitMissionCategory,
+  WellFitMissionRouteKey,
+} from "./missionCategoryTypes";
+
+export { missionCategoryDefinitions } from "./missionCategoryTypes";
+
+export type {
+  MissionDraft,
+  MissionDraftAgeBand,
+  MissionDraftCreator,
+  MissionDraftQuestion,
+  MissionDraftSourceContext,
+  MissionDraftStatus,
+  MissionDraftWaypoint,
+} from "./missionDraftTypes";
