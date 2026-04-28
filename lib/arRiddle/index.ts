b7e55cea360bff1e80d9ef5ca34719c86c0ae027
Bernds-tech <b.guggennberger@gmail.com>
2026-04-28 16:41:45 +0200

    Add AR riddle type barrel exports

---
+++ lib/arRiddle/index.ts
@@ -0,0 +1,12 @@
+export type {
+  ArEvidenceSource,
+  ArQuestionAnswerOption,
+  ArQuestionDifficulty,
+  ArQuestionType,
+  ArRepeatPenaltyLevel,
+  ArRiskLevel,
+} from "./arRiddleBaseTypes";
+
+export type { ArQuestionEvidenceEvent } from "./arQuestionEvidenceTypes";
+export type { ArQuestionMemory } from "./arQuestionMemoryTypes";
+export type { ArRewardPolicyInput, ArRewardPolicyResult } from "./arRewardPolicyTypes";
