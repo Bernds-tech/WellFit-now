12e6388a1ea7770c31412fc12cb362b1c9696abc
Bernds-tech <b.guggennberger@gmail.com>
2026-07-23 16:12:44 +0200

    Register adventure catalog and progress callables

--- functions/lib/beta1SafetyAdmin.js
+++ functions/lib/beta1SafetyAdmin.js
@@ -13,6 +13,8 @@ const { registerBeta1WeeklyMissionCatalog } = require("./beta1WeeklyMissionCatal
 const { registerBeta1WeeklyMissionProgress } = require("./beta1WeeklyMissionProgress");
 const { registerBeta1ChallengeMissionCatalog } = require("./beta1ChallengeMissionCatalog");
 const { registerBeta1ChallengeMissionProgress } = require("./beta1ChallengeMissionProgress");
+const { registerBeta1AdventureMissionCatalog } = require("./beta1AdventureMissionCatalog");
+const { registerBeta1AdventureMissionProgress } = require("./beta1AdventureMissionProgress");
 const { registerBeta1PoseEvidence } = require("./beta1PoseEvidence");
 const { registerBeta1BuddyActions } = require("./beta1BuddyActions");
 
@@ -122,6 +124,8 @@ function registerBeta1SafetyAdmin(exportsTarget, deps) {
   registerBeta1WeeklyMissionProgress(exportsTarget, deps);
   registerBeta1ChallengeMissionCatalog(exportsTarget, deps);
   registerBeta1ChallengeMissionProgress(exportsTarget, deps);
+  registerBeta1AdventureMissionCatalog(exportsTarget, deps);
+  registerBeta1AdventureMissionProgress(exportsTarget, deps);
   registerBeta1PoseEvidence(exportsTarget, deps);
   registerBeta1BuddyActions(exportsTarget, deps);
 
