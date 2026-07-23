59a74f9996cf7dc97b412a5dbd6ecb2495ddc529
Bernds-tech <b.guggennberger@gmail.com>
2026-07-23 14:13:05 +0200

    Reset tracking and review collections in Beta-1 tests

--- functions/test/beta1RuntimeFixtures.js
+++ functions/test/beta1RuntimeFixtures.js
@@ -84,6 +84,7 @@ function describeCall(callResponse) {
 async function resetBeta1Collections() {
   const collections = [
     "familyAccounts", "childProfiles", "guardianChildLinks", "parentalConsents", "missions", "missionAttempts", "missionEvidence", "missionCompletions",
+    "trackingSessions", "trackingProofEvents", "missionBuddyEvents", "missionContextEvaluations", "missionCompletionEvaluations", "missionRewardPreviews", "missionEvidenceReviews", "missionPatternReviews", "missionCooldownReviews", "missionRewardEvents",
     "userDailyMissionState", "userDailyStreaks", "userLevels", "xpWallets", "xpLedgerEvents", "ledgerEvents", "auditEvents", "adminActions", "itemDefinitions", "shopItems", "shopPurchaseIntents", "shopPurchaseEvents", "userInventory", "userAvatars",
     "checkpoints", "checkpointScores", "checkpointMayors", "mayorShareEvents", "glitchEvents", "glitchParticipants", "glitchBoostWindows", "glitchSafetyRules", "safetyReports",
   ];
